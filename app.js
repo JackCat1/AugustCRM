a705ad01f612268aa1cc458e01358501eaadc768
JackCat1 <kotov.maksim1989@gmail.com>
2018-08-02 13:21:54 +0600

    Add utilits Cors, Morgan

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const express=require('express');
+const app=express();
+const bodyParser=require('body-parser');
 const authRouter=require('./routers/auth');
 const analyticRouter=require('./routers/analytics');
 const categotyRouter=require('./routers/categoty');
 const orderRouter=require('./routers/order');
 const positionRouter=require('./routers/positions');
-const app=express();
 
+//Utilits
+app.use(require('cors')());
+app.use(require('morgan')('dev'));
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
 
+//Routers
 app.use('/api/auth', authRouter);
 app.use('/api/analytics',analyticRouter);
 app.use('/api/categoty',categotyRouter);

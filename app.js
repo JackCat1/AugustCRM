27fe2854d2492cb17f92888fcabd8ddc0b4bce2a
JackCat1 <kotov.maksim1989@gmail.com>
2018-08-03 14:41:22 +0600

    add config

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express=require('express');
 const app=express();
+const mongoose = require('mongoose');
 const bodyParser=require('body-parser');
 const authRouter=require('./routers/auth');
 const analyticRouter=require('./routers/analytics');
 const categotyRouter=require('./routers/categoty');
 const orderRouter=require('./routers/order');
 const positionRouter=require('./routers/positions');
+const keys = require('./config/keys');
+
+// DB connect
+mongoose.connect(keys.mongoURI)
+    .then(()=> console.log('DB is connect'))
+    .catch(error => console.log(error));
 
 //Utilits
 app.use(require('cors')());
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb://JackCat1:vfrcbv17@ds111192.mlab.com:11192/augustcrm'
+}

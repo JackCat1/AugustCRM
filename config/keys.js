d5a37b6e1fd39145f3e146c0956846b45773f8b3
JackCat1 <kotov.maksim1989@gmail.com>
2018-08-03 16:34:13 +0600

    Auth + token

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb://JackCat1:vfrcbv17@ds111192.mlab.com:11192/augustcrm'
+    mongoURI: 'mongodb://JackCat1:vfrcbv17@ds111192.mlab.com:11192/augustcrm',
+    jwt: '1111'
 }
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,14 +1,66 @@
-module.exports.login = (req,res)=>{
-    res.status(200).json({
-        login:{
-            email:req.body.email,
-            password:req.body.password
+const bycrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const User = require('../models/User');
+const keys = require('../config/keys')
+
+module.exports.login = async (req,res)=>{
+    //Login
+    const tempUser = await User.findOne({email: req.body.email});
+    if(tempUser){
+        // User is found
+        const passwordResult = bycrypt.compareSync(req.body.password,tempUser.password);
+        if(passwordResult){
+            //Make token
+            const token = jwt.sign({
+                email:tempUser.email,
+                userId: tempUser._id
+            },keys.jwt, {expiresIn: 60*60});
+
+            res.status(200).json({
+                token: `Bearer ${token}`
+            });
+
+        } else {
+            res.status(401).json({
+                message: 'Вы ввели неверный пароль. Попробуйте еще раз.'
+            })
         }
-    })
+
+
+    } else{
+        // User not found
+        res.status(404).json({
+            message: 'Пользовательс таким email не найден. Проверьте правильность email.'
+        })
+
+    }
 }
 
-module.exports.reg= (req,res)=>{
-    res.status(200).json({
-        reg:'controller'
-    })
+module.exports.reg = async (req,res)=>{
+    // Find in DB email
+    const tempUser = await User.findOne({email: req.body.email});
+    if (tempUser){
+        res.status(409).json({
+            message: 'Такой email уже занят. Попробуйте другой!'
+        })
+    }
+    else {
+        const salt = bycrypt.genSaltSync(10);
+        const password = req.body.password;
+
+        const user = new User({
+            email: req.body.email,
+            password: bycrypt.hashSync(password,salt)
+        });
+
+        try{
+            await user.save();
+            res.status(201).json(user);
+        } catch(e){
+            // Обработать ошибку
+        }
+        
+
+    }
 }
